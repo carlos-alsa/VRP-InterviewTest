47a4c59b03168f1e25277ab36288b5558a41cddb
Carlos Sanabria <carlos-alsa@hotmail.com>
2025-04-28 18:48:56 -0500

    Added new updates to the project

--- src/test/java/seleniumgluecode/Hooks.java
+++ src/test/java/seleniumgluecode/Hooks.java
@@ -2,7 +2,9 @@ package seleniumgluecode;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.utils.PropertyManager;
 import java.util.logging.Logger;
 
 /**
@@ -10,19 +12,16 @@ import java.util.logging.Logger;
  */
 
 public class Hooks {
-
     private static ChromeDriver driver;
-    private String TYPE_DRIVER = "webdriver.chrome.driver";
-    private String PATH_DRIVER = "./src/test/resources/chromedriver/chromedriver.exe";
-    private String URL = "https://business-dream-3139.my.salesforce.com/";
-    private Logger log = Logger.getLogger(Hooks.class.getName());
+    private final String baseUrl = PropertyManager.getAuthValueByKey("URL");
+    private final Logger log = Logger.getLogger(Hooks.class.getName());
 
     @Before
     public void setUp(){
-        System.setProperty(TYPE_DRIVER,PATH_DRIVER);
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get(URL);
         driver.manage().window().maximize();
+        driver.get(baseUrl);
         log.info("");
 
     }
